d07ca8c55881dfbeaa738b8994d838f69e828251
ceasercipher <40567184+nkendrick101@users.noreply.github.com>
2023-05-31 13:52:03 -0500

    ui updates

--- src/components/RepositoryCard.tsx
+++ src/components/RepositoryCard.tsx
@@ -9,8 +9,11 @@ interface Props {
 function RepositoryCard({ repository }: Props) {
   return (
  
-     
+    <Link to={`/repository/${repository.id}`} className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700">
 <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+<h2 className="mb-3 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">{repository.name}</h2>
+            <p className="mb-3 text-gray-500 dark:text-gray-400">{repository.description}</p>
+     
     <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 rounded-t-lg bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800" id="defaultTab" data-tabs-toggle="#defaultTabContent" role="tablist">
         <li className="mr-2">
         Owner: {repository.owner}
@@ -22,20 +25,11 @@ function RepositoryCard({ repository }: Props) {
         Forks: {repository.forks}
         </li>
     </ul>
-    <div id="defaultTabContent">
-        <div className="hidden p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800" id="about" role="tabpanel" aria-labelledby="about-tab">
-            <h2 className="mb-3 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">{repository.name}</h2>
-            <p className="mb-3 text-gray-500 dark:text-gray-400">{repository.description}</p>
-            <Link to={`/repository/${repository.id}`} className="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700">
-                view
-                <svg className="w-6 h-6 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-            </Link>
-        </div>
+   
        
-    
-    </div>
+  
 </div>
-
+</Link>
   );
 }
 
--- src/pages/RepositoryPage.tsx
+++ src/pages/RepositoryPage.tsx
@@ -47,17 +47,29 @@ function RepositoryPage() {
 
   return (
     <div>
-   <div className="p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
-  <span className="font-medium">Info alert!</span> Change a few things up and try submitting again.
-</div>
+ 
+ <h2 className="text-4xl font-extrabold text-gray-900 ">{repository.name}</h2>
+<p className="my-4 text-lg text-gray-500">{repository.description}</p>
 
-      <h1>{repository.name}</h1>
-      <p>{repository.description}</p>
+    
       <ul>
-        <li>Owner: {repository.owner}</li>
-        <li>Stars: {repository.stars}</li>
-        <li>Forks: {repository.forks}</li>
+      <li  className="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">
+  
+    <span className="w-full">Owner: {repository.owner}</span>
+   
+</li>
+        
+        <li  className="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">
+        <span className="w-full">
+        Stars: {repository.stars}
+        </span>
+        </li>
+        <li  className="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">
+        <span className="w-full"> Forks: {repository.forks}</span>
+        </li>
+       
       </ul>
+
     </div>
   );
 }
