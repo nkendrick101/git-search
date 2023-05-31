979fbe9243edf053ee40a2ae96f1897d6efe6436
ceasercipher <40567184+nkendrick101@users.noreply.github.com>
2023-05-31 13:41:56 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,9 +20,6 @@ function Header() {
       
       </div>
       <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-        <a href="/" className="text-sm font-semibold leading-6 text-gray-900">
-        
-          Repo Lookup <span aria-hidden="true">→</span></a>
         
     
       </div>
